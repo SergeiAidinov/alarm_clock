81f5ab4045e3cd8a910c20b79653bd425f40e593
Sergei Aidinov <incoming34@yandex.ru>
2024-12-04 00:03:28 +0300

    works correctly

--- src/alarm_clock.c
+++ src/alarm_clock.c
@@ -16,27 +16,27 @@
 int main(int argc, char * argv[]) {
 	initscr();
 	curs_set(0);
-	char message = argv[1];
+	int alarm_hour = atoi(argv[1]);
+	int alarm_minute = atoi(argv[2]);
 	int hour, minute, second;
-	//time_t init_time = time(NULL);
 	do {
 		time_t mytime = time(NULL);
 		struct tm *now = localtime(&mytime);
-		/*long tm_sec_diff = (mktime(&mytime) - mktime(&init_time));
-		printf("%d ==> ", tm_sec_diff);*/
 		hour = now->tm_mday;
 		minute = now->tm_min;
 		second = now->tm_sec;
-		puts(message);
+		printw("Alarm clock is set for %d hour(s) %d minute(s)\n", alarm_hour, alarm_minute);
 		printw("Date: %d.%d.%d\n", now->tm_mday, now->tm_mon + 1,
 				now->tm_year + 1900);
-		//printw("Time: %d:%d:%d\n", now->tm_hour, now->tm_min, now->tm_sec);
 		printw("Time: %d:%d:%d\n", hour, minute, second);
 		refresh();
 		clear();
 		sleep(1);
-		/*system(
-				"play -n synth \
+		if (hour >= alarm_hour && minute >= alarm_minute) break;
+
+	} while (1);
+	system(
+					"play -n synth \
 					pl F4  pl A4  pl C5  pl F5  pl C4  pl G4  pl C5  pl E5  pl D4  pl F4  \
 					pl A4  pl D5  pl A3  pl F4  pl A4  pl C5  pl F5  pl C4  pl G4  pl C5  \
 					pl E5  pl D4  pl F4  pl A4  pl D5  pl A3  pl E4  pl A4  pl C5  pl Bb3 \
@@ -57,8 +57,8 @@ int main(int argc, char * argv[]) {
 					+0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 \
 					+0.700 +0.700 +0.700 +0.700 +0.700 +0.700 +0.700 +0.700 \
 					\
-					remix - fade 0 20 .1 norm -1");*/
-	} while (1);
-
+					remix - fade 0 20 .1 norm -1");
+	refresh();
+	clear();
 	return EXIT_SUCCESS;
 }
